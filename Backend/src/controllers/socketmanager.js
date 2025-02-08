d78fc139291cd3a8e4f4a95d811c46284b4b1325
Kunj Tyagi <kunjtyagi24@gmail.com>
2025-02-08 19:48:33 +0530

    Socket for live interaction

--- Backend/src/controllers/socketmanager.js
+++ Backend/src/controllers/socketmanager.js
@@ -1,7 +1,114 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 
-export const connectToSocket=(server)=>{
-    const io=new Server(server);
-    
-    return io;
-}
+let connections = {}; // Kitna log in hai meeting mai
+let messages = {};
+let timeOnline = {};
+
+export const connectToSocket = (server) => {
+  const io = new Server(server);
+
+  //jab bhi socket se koi connect hota hai,toh ye event fire hota hai io.on("connection")
+  io.on("connection", (socket) => {
+    socket.on("join-call", (path) => {
+      if (connections[path] === undefined) {
+        connections[path] = [];
+      }
+      connections[path].push(socket.id);
+
+      timeOnline[socket.id] = new Date();
+
+      //   connections[path].forEach(elem=>{
+      //     io.to(elem)
+      //   })
+
+      for (let a = 0; a < connections[path].length; ++a) {
+        io.to(connections[path][a]).emit(
+          "user-joined",
+          socket.id,
+          connections[path]
+        );
+      }
+
+      if (messages[path] !== undefined) {
+        for (let a = 0; a < messages[path].length; ++a) {
+          io.to(socket.id).emit(
+            "chat-message",
+            messages[path][a]["data"],
+            messages[path][a]["sender"],
+            messages[path][a]["socket-id-sender"]
+          );
+        }
+      }
+    });
+
+    // A user sends a signal (message) to another user by providing their socket ID.
+    // The server receives this signal and forwards it to the correct user.
+    socket.on("signal", (toId, message) => {
+      io.to(toId).emit("signal", socket.id, message);
+    });
+
+    // Client side pe kisi ne Hi--Likh-kar enter kiya toh waha pe emit ho raha hoga.
+    //  Toh yaha pe chat-message ayi hai hamara pass.
+    // Whenever a user sends a chat message, the server:
+    // a) Finds the chat room where the user belongs.
+    // b) Stores the message in that room’s message history.
+    // c) Sends the message to all users in the same room.
+    socket.on("chat-message", (data, sender) => {
+      const [matchingRoom, found] = Object.enteries(connections).reduce(
+        ([room, isFound], [roomKey, roomValue]) => {
+          if (!isFound && roomValue.includes(socket.id)) {
+            return [roomKey, true];
+          }
+          retrun[(room, isFound)];
+        },
+        ["", false]
+      );
+
+      if (found === true) {
+        if (messages[matchingRoom] === undefined) {
+          messages[matchingRoom] = [];
+        }
+
+        messages[matchingRoom].push({
+          sender: sender,
+          data: data,
+          "socket-id-sender": socket.id,
+        });
+        console.log("messages", KeyboardEvent, ":", sender, data);
+
+        connections[matchingRoom].forEach((elem) => {
+          io.to(elem).emit("chat-message", data, sender, socket.id);
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      var diffTime = Math.abs(timeOnline[socket.id] - new Date());
+
+      // key - room-Id
+      for (const [k, v] of JSON.parse(
+        JSON.stringify(Object.entries(connections))
+      )) {
+        for (let a = 0; a < v.length; ++a) {
+          if (v[a] === socket.id) {
+            let key = k;
+
+            for (let a = 0; a < connections[key].length; ++a) {
+              io.to(connections[key][a]).emit("user-left", socket.id);
+            }
+
+            var index = connections[key].indexOf(socket.id);
+
+            connections[key].splice(index, 1);
+
+            if (connections[key].length === 0) {
+              delete connections[key];
+            }
+          }
+        }
+      }
+    });
+  });
+
+  return io;
+};
